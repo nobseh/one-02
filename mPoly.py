acc2dc8fcd85f633c923842fdb41956a86ba5d3f
nobseh <hesbonnobseh@gmail.com>
2024-10-31 19:18:07 +0300

    trying git

---
+++ mPoly.py
@@ -0,0 +1,29 @@
+
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model 
+    
+    def move(self):
+        return f"Can Drive."
+
+class Car(Vehicle):
+    pass
+
+class Boat(Vehicle):
+    def move(self):
+        return f"Can Sail."
+
+class Plane(Vehicle):
+    def move(self):
+        return f"Can Fly."
+
+car1 = Car("Ford", "Mustang")
+boat1 = Boat("Ibiza", "T45")
+plane1 = Plane("Boeng", 747)
+
+for x in (car1, boat1, plane1):
+    print(x.make, x.model)
+    print(x.move())
+
+
